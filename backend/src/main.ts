c2ff6b37a111c610839f373db4672600ef05f12e
Camilo Forero <124638099+Camilo-Forero04@users.noreply.github.com>
2025-11-20 10:24:00 -0500

    arreglando: bad getaway 502

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,14 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // HABILITAR CORS: Esto permite que Next.js (3000) reciba datos de NestJS (4000)
+  // Habilitar CORS
   app.enableCors({
-    origin: 'http://localhost:3000', // Solo permitimos a tu frontend
+    origin: 'https://gacetaingles.com', // Ya que el frontend está en el dominio real
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  await app.listen(4000);
-  console.log(`🚀 Servidor corriendo en puerto 4000`);
+  // 👇 EL CAMBIO CRÍTICO: Escuchar en 0.0.0.0 para que Railway pueda entrar
+  await app.listen(process.env.PORT || 4000, '0.0.0.0'); 
 }
 bootstrap();
