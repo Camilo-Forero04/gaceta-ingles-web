cf8a3510f04b703597f6617fb1842ea724705c56
Camilo Forero <124638099+Camilo-Forero04@users.noreply.github.com>
2025-11-21 16:36:26 -0500

    Cambiando: Textura del libro

--- frontend/src/components/Book3DScene.tsx
+++ frontend/src/components/Book3DScene.tsx
@@ -8,7 +8,7 @@ import * as THREE from "three";
 
 function BookModel() {
   const meshRef = useRef<THREE.Mesh>(null);
-  const coverTexture = useTexture("/Portada.png");
+  const coverTexture = useTexture("/book_cover_texture.jpg");
   
   // Configuración para que la textura se estire bien si es necesario
   coverTexture.center.set(0.5, 0.5); 
