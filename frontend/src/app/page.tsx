52d905281b68defdc2dc430e862bcf5566c4970b
Camilo Forero <124638099+Camilo-Forero04@users.noreply.github.com>
2025-11-25 01:52:10 -0500

    borrando navbar fea

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,10 +5,6 @@ import Guarantee from "../components/Guarantee";
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
-      {/* Componente Navbar (Pendiente) */}
-      <nav className="p-4 border-b text-center text-sm text-gray-400">
-        [ Navbar: La Gaceta del Inglés ]
-      </nav>
 
       {/* Componente Hero */}
       <PresaleHero />
